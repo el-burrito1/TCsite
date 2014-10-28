74bbdb9c390b4d1a0b89c2dde5f3135ff1777e0e
Spencer Spiegel <spencer@Spencers-MacBook-Pro.local>
2014-10-28 14:22:21 -0700

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,37 @@
+$(document).on('ready' , function(){
+
+	$window = $(window);
+
+	$('[data-type]').each(function() {
+	  $(this).data('offsetY', parseInt($(this).attr('data-offsetY')));
+	  $(this).data('speed', $(this).attr('data-speed'));
+	});
+
+	$('section[data-type="background"]').each(function(){
+
+		var $self = $(this),
+		    offsetCoords = $self.offset(),
+		    topOffset = offsetCoords.top;
+
+		$(window).scroll(function(){
+
+		if ( ($window.scrollTop() + $window.height()) > (topOffset) &&
+		( (topOffset + $self.height()) > $window.scrollTop() ) ) {
+
+		  var yPos = -($window.scrollTop() / $self.data('speed'));
+
+		  if ($self.data('offsetY')) {
+		    yPos += $self.data('offsetY');
+		  }
+
+		  var coords = '50% '+ yPos + 'px';
+
+		
+		  $self.css({ backgroundPosition: coords })
+
+		  }
+
+		})
+	}); 
+
+})
